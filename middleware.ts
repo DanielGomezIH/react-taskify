94ba0a05506aeed0aeb45addf348d3b290ecb40e
Daniel Gómez <danielivgho5@gmail.com>
2024-06-25 14:44:41 -0500

    create organization page and set up redirecting routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,37 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  '/organization(.*)',
+  '/select-org(.*)',
+]);
+
+export default clerkMiddleware((auth, req: NextRequest) => {
+  // handle authenticated users who are trying to access a public route
+  if (auth().userId && !isProtectedRoute(req)) {
+    let path = '/select-org';
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
+    }
+    const orgSelection = new URL(path, req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+  // Handle not authenticated users who are trying to access a private route
+  if (!auth().userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn({ returnBackUrl: req.url });
+  }
+
+  // Handle authenticated users who doesn't have an organization and there aren't in select-org page
+  if (
+    auth().userId &&
+    !auth().orgId &&
+    req.nextUrl.pathname !== '/select-org'
+  ) {
+    const orgSelection = new URL('/select-org', req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
